2b8c76407b3c79512e8140252ced6cc529495d4c
Sujay <sujay.av555@gmail.com>
2018-05-30 14:19:21 +0530

    commit

--- Controller/HomeCtrl.js
+++ Controller/HomeCtrl.js
@@ -1,11 +1,18 @@
-app.controller('homeCtrl',function ($scope,$mdSidenav,$http) {
+app.controller('homeCtrl',function ($scope,$mdSidenav,$http,$mdDialog) {
   console.log("inside homeCtrl");
   $scope.toggleLeft = buildToggler('left');
   $http.get("assets/products.json").then(function (response) {
       $scope.myData = response.data;
       return $scope.myData;
   });
+$scope.showAlert = function(description) {
+  $mdDialog.show(
+      $mdDialog.alert()
+      .title('DESCRIPTION : '+description)
+      .ok('Got it!')
 
+     );
+};
   function buildToggler(componentId) {
     return function() {
       $mdSidenav(componentId).toggle();
