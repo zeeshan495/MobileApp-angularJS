aa582a2ae92ef643ecc5a82d667be030a61b8113
Sujay <sujay.av555@gmail.com>
2018-06-01 17:59:47 +0530

    updating

--- Controller/HomeCtrl.js
+++ Controller/HomeCtrl.js
@@ -1,29 +1,53 @@
-app.controller('homeCtrl',function ($scope,$mdSidenav,$http,$mdDialog) {
+app.controller('homeCtrl', function($scope, $mdSidenav, $http, $mdDialog) {
   console.log("inside homeCtrl");
   $scope.toggleLeft = buildToggler('left');
-  $http.get("assets/products.json").then(function (response) {
-      $scope.myData = response.data;
 
+  $http.get("assets/products.json").then(function(response) {
+    $scope.myData = response.data;
   });
-  $scope.showAdvanced = function(ev,data) {
-      $mdDialog.show({
-        controller: DialogController,
-        templateUrl: 'templates/popup.html',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        locals: { item: data },
-        clickOutsideToClose:true
-      });
-}
+  $scope.showAdvanced = function(ev, data) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'templates/popup.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      locals: {
+        item: data
+      },
+      clickOutsideToClose: true
+    });
+  }
+
   function buildToggler(componentId) {
     return function() {
       $mdSidenav(componentId).toggle();
     };
   }
+
   function DialogController($scope, item) {
-    $scope.item=item;
+    $scope.item = item;
     $scope.cancel = function() {
-    $mdDialog.cancel();
+      $mdDialog.cancel();
     };
-}
+  }
+
+  // function homeCtrl($scope) {
+  //   $scope.tempArray=[];
+  //   $scope.filterFunction=function(specs.manufacturer){
+  //      var i = $.inArray(specs.manufacturer, $scope.tempArray);
+  //      if (i > -1) {
+  //          $scope.tempArray.splice(i, 1);
+  //      } else {
+  //          $scope.tempArray.push(specs.manufacturer);
+  //      }
+  //   }
+  //   $scope.brandFilter = function(myData) {
+  //       if ($scope.tempArray.length > 0) {
+  //           if ($.inArray(myData.specs.manufacturer, $scope.tempArray) < 0)
+  //               return;
+  //       }
+  //
+  //       return myData;
+  //
+  // }
 });
---
+++ Filters/dashboardFilter.js
@@ -0,0 +1,6 @@
+app.filter('myFilter', function() {
+  return function(jsonFile, feature) {
+    
+    return newArr;
+  }
+}
